9c473ff100c3c0612f37c52004fa894216eef5cb
Allan <allan.agomesp@gmail.com>
2019-02-11 13:21:06 +0000

    add query update with status

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+   query: "" 
+  }
+  
+  updateQuery = (query) =>{
+  	this.setState(() => ({
+    	query: query.trim()
+    }))
+  }
   render() {
     return (
       <div className="App">
@@ -11,9 +20,9 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <div className="container">
-          <input type="text" placeholder="Say Something" />
+          <input type="text" placeholder="Say Something" onChange={(event) => this.updateQuery(event.target.value)}/>
           <p className="echo">Echo:</p>
-          <p>This should mirror the text you typed into the input field.</p>
+          <p>{this.state.query}</p>
         </div>
       </div>
     );
